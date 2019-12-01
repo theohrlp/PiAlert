6af21a7b350bfb37c56879c5d58c070d5e84da4e
Theo <ted@pop-os.local>
2019-12-02 01:40:43 +0200

    feat: Email works, need to secure credentials.

--- PiScanClient.py
+++ PiScanClient.py
@@ -1,13 +1,37 @@
 #!/usr/bin/python3          # This is client.py file
 
 import socket               # Import socket module
-import threading
 import time
+import os
+import smtplib
 
 # creates socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 flag = True
+
 port = 5456
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+def notifyUser():
+    print("Hello")
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.login("theotriton@gmail.com", "eimaimiarozarkoudapouxorebei12")
+    server.sendmail("theotriton@gmail.com", "theotriton@gmail.com", "this message is from python")
+    server.quit()
+    # with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        # smtp.ehlo()
+        # smtp.starttls()
+        # smtp.ehlo()
+    # smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    # subject = 'YOUR SITE IS DOWN!'
+    # body = 'Make sure the server restarted and it is back up'
+    # msg = f'Subject: {subject}\n\n{body}'
+    # temp = msg.as_string()
+    # smtp.sendmail(EMAIL_ADDRESS, 'EMAIL_ADDRESS', temp)
+    print("Hello")
 
 while(flag):
     # time.sleep(1)
@@ -24,15 +48,7 @@ while(flag):
         flag = False
         pass
         
-
-if(not flag):
-    {
-        print("Server is out")
-    }
+notifyUser()
 
 
-# def checkIfAlive():
-    
-
-# t1 = threading.Thread(target=checkIfAlive)
 
